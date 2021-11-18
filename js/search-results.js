1d37bce84ed9aa89066a0a2e24b3876db8835d50
jmendonca19 <jmendonca@udesa.edu.ar>
2021-11-17 22:22:24 -0300

    busqueda de resultados

--- js/search-results.js
+++ js/search-results.js
@@ -31,23 +31,47 @@ inputField.addEventListener('focus',function() {
 
 let query = location.search;
 let qsObject = new URLSearchParams(query);
-let palabra = qsObject.get('buscador')
+let palabra = qsObject.get('buscador') //estoy seleccionando solamente la palabra que buscó la persona 
 
-console.log(query);
-    
-/* let url = `https://api.themoviedb.org/3/search/movie?&api_key=924a6f16470b17afdd20524ec31c09be${palabra}`
 
+const key = "924a6f16470b17afdd20524ec31c09be";
+let url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=es&query=${palabra}`;
+
+
+let mensajeResultado = document.querySelector(".mensajeResultado")
+let mensajeBusqueda = document.querySelector(".mensajeBusqueda");
+let sectionResultadoBusqueda = document.querySelector(".peliculas");
+
+const img = 'https://image.tmdb.org/t/p/w342'
+
+let resultados = []; //array para meteer los resultados 
 
 fetch(url) 
     .then(function(response){
         return response.json();
     })
     .then(function(data){
-        console.log(data);
+        let info = data.results;
+        console.log(info);
+
+        if(info.length == 0){
+         mensajeResultado.innerText +=  "No hay resultados para su búsqueda";
+
+        } else {
+            for (let i = 0; i < info.length; i++) {
+                resultados += `<a class="linkGenero" href="detail-movie.html?id=${info[i].id}">
+                <article class="card">
+                        <img src="${img + info[i].poster_path}" alt="Foto">
+                        <h2>${info[i].original_title}</h2>
+                </article>`
+            
+                sectionResultadoBusqueda.innerHTML = resultados; 
+             }  }
+        
     })
     .catch(function(error){
         console.log(error);
     })
- */
+
 
 
